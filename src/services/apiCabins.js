fc65b4dae76f7f0f501f2b375db503685b8379d9
Khurshid Khan <khurshidsunny45@gmail.com>
2023-09-11 11:58:44 -0700

    connected our react app with the supabase database

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("The cabins could not be loaded");
+  }
+
+  return data;
+}
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://kmatyrjalsphuypxgchx.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttYXR5cmphbHNwaHV5cHhnY2h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ0MDk3MzgsImV4cCI6MjAwOTk4NTczOH0.as2I9YdaICbCZ4O4VH4ftl823j011cuL0gajLUMkv_I";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
