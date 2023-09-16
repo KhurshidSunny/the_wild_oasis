573f7fee32c1a611ce0c92daf9757f94d1528976
Khurshid Khan <khurshidsunny45@gmail.com>
2023-09-16 04:56:15 -0700

    converted the Modal window to the compound component to reuse it

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,23 +1,43 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
-  const [isShowModal, setIsShowModal] = useState(false);
-  function handlCloseModal() {
-    setIsShowModal(false);
-  }
   return (
-    <div>
-      <Button onClick={() => setIsShowModal((show) => !show)}>Add Cabin</Button>
-      {isShowModal && (
-        <Modal onCloseModal={handlCloseModal}>
-          <CreateCabinForm onCloseModal={handlCloseModal} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opensWindowName="cabin-form">
+        <Button>Add new Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opensWindowName="table">
+        <Button>show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isShowModal, setIsShowModal] = useState(false);
+//   function handlCloseModal() {
+//     setIsShowModal(false);
+//   }
+//   return (
+//     <div>
+//       <Button onClick={() => setIsShowModal((show) => !show)}>Add Cabin</Button>
+//       {isShowModal && (
+//         <Modal onCloseModal={handlCloseModal}>
+//           <CreateCabinForm onCloseModal={handlCloseModal} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
