69c20c70b0b15a8722851a457ba1bc5ea2a713ec
Khurshid Khan <khurshidsunny45@gmail.com>
2023-09-17 21:14:23 -0700

    implement the feature of closing the window by clicking outside the window

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -3,7 +3,15 @@
 import styled from "styled-components";
 import { HiXMark } from "react-icons/hi2";
 import { createPortal } from "react-dom";
-import { cloneElement, createContext, useContext, useState } from "react";
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -81,11 +89,12 @@ function Open({ children, opensWindowName }) {
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
 
-  if (name !== openName) return null;
+  const ref = useOutsideClick(close);
 
+  if (name !== openName) return null;
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
