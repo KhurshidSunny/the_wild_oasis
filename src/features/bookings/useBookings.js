007846edbe54875608563a4f51c2986adf5e6a9c
Khurshid Khan <khurshidsunny45@gmail.com>
2023-09-30 04:32:37 -0700

    prefetched booking data from supabase database to the pagination component using react query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings as getBookingsApi } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/Constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
+
   // 1. FILTER
   const [searchParams] = useSearchParams();
   const filterValue = searchParams.get("status");
@@ -20,6 +23,7 @@ export function useBookings() {
 
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // 1. USEQUERY
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -28,6 +32,24 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookingsApi({ filter, sortBy, page }),
   });
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  // PRE-FETCHING
+
+  // i. for next button prefetching
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookingsApi({ filter, sortBy, page: page + 1 }),
+    });
+
+  // ii. for previous button prefetching
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookingsApi({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, bookings, error, count };
 }
