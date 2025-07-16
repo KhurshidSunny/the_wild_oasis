13fefbef4d09a2d4c149742fe7302b5e2eba021a
KhurshidSunny <khurshidsunny45@gmail.com>
2025-07-16 12:30:11 +0500

    updated the restrict middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -87,8 +87,10 @@ exports.login = catchAsync(async (req, res, next) => {
 exports.protect = catchAsync(async(req, res, next) => {
     let token;
     // 1) Getting token and check if it's there
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
     }
 
     if(!token) {
