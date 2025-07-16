31c3d98db9fc989e10fe430bbc2b838b14399db0
KhurshidSunny <khurshidsunny45@gmail.com>
2025-07-16 13:12:15 +0500

    added log in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -92,6 +92,7 @@ exports.protect = catchAsync(async(req, res, next) => {
     } else if (req.cookies.jwt) {
         token = req.cookies.jwt;
     }
+    console.log(token)
 
     if(!token) {
         return next(new AppError('You are not logged in. Please login to get access', 401))
