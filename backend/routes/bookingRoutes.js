452678cff66ef41ff218c6b4d2adede516900eb9
KhurshidSunny <khurshidsunny45@gmail.com>
2025-07-30 17:02:48 +0500

    fix get booking route

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -2,19 +2,22 @@ const express = require("express");
 const bookingController = require("../controllers/bookingController");
 
 const router = express.Router();
+
+// ✅ Move more specific routes first
+router.get("/guest/:guest_id", bookingController.getBookingsForGuest);
 router.get("/getBookingsAfterDate", bookingController.getBookingsAfterDate);
 router.get("/getStaysTodayActivity", bookingController.getStaysTodayActivity);
 router.get("/getStaysAfterDate", bookingController.getStaysAfterDate);
 
-router.get("/guest/:guest_id", bookingController.getBookingsForGuest);
-router
-  .route("/")
-  .get(bookingController.getBookings)
-  .post(bookingController.createBooking);
 router
   .route("/:booking_id")
   .get(bookingController.getBooking)
   .patch(bookingController.updateBooking)
   .delete(bookingController.deleteBooking);
+
+router
+  .route("/")
+  .get(bookingController.getBookings)
+  .post(bookingController.createBooking);
 
 module.exports = router;
